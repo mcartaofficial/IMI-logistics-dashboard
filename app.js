f06538e6ebe8bee0a008d01544085a4bc04976cf
mcartaofficial <michael.cartagenajr@yahoo.com>
2026-04-12 11:38:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ class MILogisticsApp {
             },
             elfsight: {
                 width: "100%",
-                height: "550px" 
+                height: "900px" 
             }
         };
         // -----------------------------
