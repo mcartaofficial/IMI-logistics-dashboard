3bc66e6b4d08b93e1d951cedc5be0c762b5ec4c8
mcartaofficial <michael.cartagenajr@yahoo.com>
2026-04-21 12:27:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -48,17 +48,17 @@ class MILogisticsApp {
                 </div>
                 <div class="service-box">
                     <h4>Sourcing</h4>
-                    <p>IMI holds multi-year contracts and marketing rights with suppliers for many products including natural gypsum from Oman, Spain and Mexico; bauxite from Australia and Turkey; and cement/clinker worldwide.</p>
+                    <p>IMI holds multi-year contracts and marketing rights with suppliers for many products including natural gypsum from Oman, Spain and Mexico; bauxite from Australia and Turkey; cement and clinker from Europe, Latin America and Asia; iron ore-related products from Trinidad, USA, Egypt, Persian Gulf and South America; and solid fuels from South America, USA, South Africa and Asia.</p>
                     <button class="read-me-btn">Read Me</button>
                 </div>
                 <div class="service-box">
                     <h4>Chartering</h4>
-                    <p>Through our in-house Chartering and Traffic operations departments, IMI has a first class reputation as a reliable expert in ocean vessel chartering and logistics planning.</p>
+                    <p>Through our in-house Chartering and Traffic operations departments, IMI has a first class reputation as a reliable expert in ocean vessel chartering, import/export operations, ship loading and discharge operations, and logistics planning and management.</p>
                     <button class="read-me-btn">Read Me</button>
                 </div>
                 <div class="service-box">
                     <h4>Logistics</h4>
-                    <p>IMI manages +50 global stock-and-sell centers for coal, gypsum, bauxite, slag, iron ore products and clinker, supporting door-to-door deliveries.</p>
+                    <p>IMI manages +50 global stock-and-sell centers for coal, gypsum, bauxite, slag, iron ore products and clinker. IMI negotiates and manages port leases and offsite stockpiles to support door-to-door deliveries.</p>
                     <button class="read-me-btn">Read Me</button>
                 </div>
                 <div class="service-box">
@@ -68,7 +68,7 @@ class MILogisticsApp {
                 </div>
                 <div class="service-box">
                     <h4>Service and Support</h4>
-                    <p>IMI’s technical and trade support capabilities are unmatched in the industry, providing custom financing solutions to suppliers and customers.</p>
+                    <p>IMI’s technical and trade support capabilities are unmatched in the industry and the “high-touch” approach ensures accurate, on-time delivery of product.</p>
                     <button class="read-me-btn">Read Me</button>
                 </div>
             </div>
@@ -81,11 +81,11 @@ class MILogisticsApp {
                 <ul class="commitment-list">
                     <li class="commitment-item">
                         <div class="red-bullet"></div>
-                        <div class="commitment-text">We work toward sourcing our raw materials from producers who engage in responsible mining processes.</div>
+                        <div class="commitment-text">We work toward sourcing our raw materials from producers who engage in responsible mining processes. This includes implementing measures to reduce ecological disruption and enhancing resource efficiency.</div>
                     </li>
                     <li class="commitment-item">
                         <div class="red-bullet"></div>
-                        <div class="commitment-text">By selecting suppliers dedicated to these principles, we aim to support more sustainable resource management.</div>
+                        <div class="commitment-text">By selecting suppliers dedicated to these principles, we aim to support efforts that contribute to more sustainable resource management within the industry.</div>
                     </li>
                     <li class="commitment-item">
                         <div class="red-bullet"></div>
@@ -102,22 +102,77 @@ class MILogisticsApp {
                     <div class="hq-box">
                         <h4>Florida | USA</h4>
                         <p>54 SE 5th Avenue, Suite 300<br>Delray Beach, FL 33483</p>
-                        <a href="mailto:operations.amer@imigroup.com">operations.amer@imigroup.com</a>
+                        <p><span class="hq-label">Toll Free</span><a href="tel:+18774230226">+1-877-423-0226</a></p>
+                        <p><span class="hq-label">Phone</span><a href="tel:+15617050350">+1-561-705-0350</a></p>
+                        <p><span class="hq-label">Fax</span><a href="fax:+15613910546">+1-561-391-0546</a></p>
+                        <p><a href="mailto:operations.amer@imigroup.com">operations.amer@imigroup.com</a></p>
+                        <a href="#" class="hq-link">Map & Directions</a>
+                    </div>
+                    <div class="hq-box">
+                        <h4>Belo Horizonte | Brazil</h4>
+                        <p>IMI Brasil Trading Ltda<br>Rua Antônio de Albuquerque, 194/sl. 902<br>Savassi – Belo Horizonte, MG, BRAZIL</p>
+                        <p><span class="hq-label">Phone</span><a href="tel:+5531991828338">+55 (31) 99182-8338</a></p>
+                        <p><a href="mailto:operations.latam@imigroup.com">operations.latam@imigroup.com</a></p>
+                    </div>
+                    <div class="hq-box">
+                        <h4>Dubai | UAE</h4>
+                        <p>IMI Middle East Trading L.L.C.<br>Offices 801 & 802, Tower A, The Offices, One Za’abeel, Za’abeel Palace St, Dubai, U.A.E</p>
+                        <p><span class="hq-label">Phone</span><a href="tel:+97147760100">+971-4-776-0100</a></p>
+                        <p><a href="mailto:operations.asia@imigroup.com">operations.asia@imigroup.com</a></p>
+                        <a href="#" class="hq-link">Map & Directions</a>
                     </div>
                     <div class="hq-box">
                         <h4>Madrid | Spain</h4>
                         <p>IMI Europe, SLU<br>Calle Velázquez 123. 2º PL, 28006 Madrid, Spain</p>
-                        <a href="mailto:operations.eur@imigroup.com">operations.eur@imigroup.com</a>
+                        <p><span class="hq-label">Phone</span><a href="tel:+34915641045">+34-915-64-1045</a></p>
+                        <p><a href="mailto:operations.eur@imigroup.com">operations.eur@imigroup.com</a></p>
+                        <a href="#" class="hq-link">Map & Directions</a>
                     </div>
                     <div class="hq-box">
-                        <h4>Dubai | UAE</h4>
-                        <p>IMI Middle East Trading L.L.C.<br>The Offices, One Za’abeel, Za’abeel Palace St, Dubai, U.A.E</p>
-                        <a href="mailto:operations.asia@imigroup.com">operations.asia@imigroup.com</a>
+                        <h4>Shanghai | China</h4>
+                        <p>Shanghai Representative Office, Suite 306B, Skybridge Plaza, Jinzhong Road 968#, Changning, Shanghai, China</p>
+                        <p><span class="hq-label">Phone</span><a href="tel:+8618217739459">+86-182-1773-9459</a></p>
+                        <p><a href="mailto:operations.asia@imigroup.com">operations.asia@imigroup.com</a></p>
+                        <a href="#" class="hq-link">Map & Directions</a>
                     </div>
                     <div class="hq-box">
                         <h4>Singapore</h4>
                         <p>IMI Trading Singapore Pte Ltd<br>1 George Street, #23-04, Singapore 049145</p>
-                        <a href="mailto:operations.asia@imigroup.com">operations.asia@imigroup.com</a>
+                        <p><span class="hq-label">Phone</span><a href="tel:+6563035680">+65-6303-5680</a></p>
+                        <p><span class="hq-label">Fax</span><a href="fax:+6563035681">+65-6303-5681</a></p>
+                        <p><a href="mailto:operations.asia@imigroup.com">operations.asia@imigroup.com</a></p>
+                        <a href="#" class="hq-link">Map & Directions</a>
+                    </div>
+                    <div class="hq-box">
+                        <h4>Mexico City | Mexico</h4>
+                        <p>Avenida Paseo de la Reforma Número 404, Piso 13, Interior 102. Colonia Juárez, Mexico City, 06600</p>
+                        <p><a href="mailto:operations.latam@imigroup.com">operations.latam@imigroup.com</a></p>
+                        <a href="#" class="hq-link">Map & Directions</a>
+                    </div>
+                    <div class="hq-box">
+                        <h4>Bogotá | Colombia</h4>
+                        <p>Calle 97A, #8-10, Oficina 503, Bogotá 110221</p>
+                        <p><a href="mailto:imicol@imigroup.com">imicol@imigroup.com</a></p>
+                        <a href="#" class="hq-link">Map & Directions</a>
+                    </div>
+                    <div class="hq-box">
+                        <h4>Johannesburg | South Africa</h4>
+                        <p>Workshop 17 The Bank, Corner Tyrwhitt Road and 24 Cradock Ave, Johannesburg, 2196</p>
+                        <p><a href="mailto:operations.asia@imigroup.com">operations.asia@imigroup.com</a></p>
+                    </div>
+                    <div class="hq-box">
+                        <h4>Istanbul | Turkey</h4>
+                        <p>Kampanaki Mansion, Köybaşı Cad. No:63, Yeniköy 34464 Sarıyer, Istanbul, Turkey</p>
+                        <p><span class="hq-label">Phone</span><a href="tel:+902122625151">+90 212 262 51 51</a></p>
+                        <p><a href="mailto:operations.eur@imigroup.com">operations.eur@imigroup.com</a></p>
+                        <a href="#" class="hq-link">Map & Directions</a>
+                    </div>
+                    <div class="hq-box">
+                        <h4>London | UK</h4>
+                        <p>International Materials UK Ltd, Office 102, 81 Fulham Road, London SW3 6RD, UK</p>
+                        <p><span class="hq-label">Phone</span><a href="tel:+442038260003">+44 20 3826 0003</a></p>
+                        <p><a href="mailto:operations.eur@imigroup.com">operations.eur@imigroup.com</a></p>
+                        <a href="#" class="hq-link">Map & Directions</a>
                     </div>
                 </div>
             </div>
@@ -236,7 +291,7 @@ class MILogisticsApp {
                     </div>
                     <div class="img-about-text">
                         <p>Since 1987, the worldwide cement, wallboard, and steel industries have come to depend on IMI to deliver unparalleled expertise to meet their raw material needs. Our commitment to service excellence has allowed us to build a large portfolio of reliable, first-class suppliers, ship operators, and customers.</p>
-                        <p>Today, IMI sources or ships to over 80 countries worldwide. Along with our partners and affiliates, we deliver over 38 million tons of bulk materials annually, providing creative and economical real-time solutions to any logistical challenge our customers may face.</p>
+                        <p>Today, IMI sources or ships to over 80 countries worldwide. Along with our partners and affiliates, we deliver over 38 million tons of bulk materials annually, providing creative and economical real-time solutions to any logistical challenge our customers may face. Our products include natural gypsum, bauxite, iron ore and iron fines, cement, clinker, coal, petcoke, slag, and copper concentrates.</p>
                     </div>
                 </div>
             `;
